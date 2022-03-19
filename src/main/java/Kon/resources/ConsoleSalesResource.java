78286b3c0fea704668f6e63d52983dbbf41308b3
Konstan <55129891+1800Kon@users.noreply.github.com>
2022-03-19 17:44:18 +0100

    re-add all schemas and add xml support

--- src/main/java/Kon/resources/ConsoleSalesResource.java
+++ src/main/java/Kon/resources/ConsoleSalesResource.java
@@ -6,6 +6,8 @@ import Kon.repositories.ConsoleSalesRepository;
 import Kon.services.ConsoleSalesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
@@ -18,22 +20,22 @@ public class ConsoleSalesResource {
     ConsoleSalesService consoleSalesService;
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public ConsoleSales save(final ConsoleSalesRequest consoleSalesRequest) {
         return consoleSalesService.save(consoleSalesRequest);
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Collection<ConsoleSales> getAll() {
         return consoleSalesService.getAll();
     }
 
     @PUT
     @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public ConsoleSales update(@PathParam("id") final Integer id, final ConsoleSalesRequest consoleSalesRequest) {
         return consoleSalesService.update(id, consoleSalesRequest);
     }
